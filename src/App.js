4256fd5b6de3f327eedd632dbbdbc8689721de84
Ramiro Contreras León <racramiro@gmail.com>
2020-10-19 17:59:50 -0500

    reset

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
-import { Router, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Recipe from "./pages/Recipe";
-import Timer from "./pages/Timer";
-import "./App.css";
+import React from 'react'
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import Home from './pages/Home'
+import Recipe from './pages/Recipe'
+import Timer from './pages/Timer'
+import './App.css'
 
-import { createBrowserHistory } from "history";
-import ReactGA from "react-ga";
-
-const history = createBrowserHistory();
-
-ReactGA.initialize("UA-00000-01");
-ReactGA.pageview(windows.location.pathname + windows.location.search);
-
-history.listen(function (location) {
-  ReactGA.pageview(windows.location.pathname + windows.location.search);
-});
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router history={history}>
+      <BrowserRouter>
         <div>
           <header>
             <Link to="/">Recetas</Link>
@@ -32,7 +21,7 @@ export default class App extends React.Component {
             <Route path="/timer" component={Timer} />
           </main>
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }

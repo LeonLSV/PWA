c7aa2b81af31a47a89e4e2873ada5358e581a1f6
Ramiro Contreras León <racramiro@gmail.com>
2020-10-19 18:42:14 -0500

    api share

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,58 +1,78 @@
-import React from 'react';
-import { Helmet } from 'react-helmet'
-import mealdb from '../mealdb-api'
-import RecipeIngredients from '../components/RecipeIngredients'
-import RecipeInstructions from '../components/RecipeInstructions'
+import React from "react";
+import { Helmet } from "react-helmet";
+import mealdb from "../mealdb-api";
+import RecipeIngredients from "../components/RecipeIngredients";
+import RecipeInstructions from "../components/RecipeInstructions";
 
 export default class Recipe extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.state = { recipe: null, isLoading: true }
+    super(props);
+    this.state = { recipe: null, isLoading: true };
   }
 
   async componentDidMount() {
-    var recipe = null
+    var recipe = null;
     try {
-      recipe = await mealdb.getRecipe(this.props.match.params.recipeId)
-    } catch(e) {
-      recipe = null
+      recipe = await mealdb.getRecipe(this.props.match.params.recipeId);
+    } catch (e) {
+      recipe = null;
     }
-    this.setState({ recipe, isLoading: false })
+    this.setState({ recipe, isLoading: false });
   }
+
+  compartir = (e) => {
+    e.preventDefault();
+    if (!navigator.share) {
+      alert("Tu Browser no soporta la web Share");
+      return;
+    }
+
+    const { recipe } = this.state;
+
+    navigator
+      .share({
+        title: `${recipe.name}`,
+        text: "Receta de Platzi",
+        url: document.location.href,
+      })
+      .then(() => alert("contenido compartido!!"))
+      .catch((error) => alert("Hubo un error!"));
+  };
 
   render() {
-    const { recipe, isLoading } = this.state
+    const { recipe, isLoading } = this.state;
 
-    if( isLoading ) {
-      return <div className="message">Cargando...</div>
+    if (isLoading) {
+      return <div className="message">Cargando...</div>;
+    } else if (recipe === null) {
+      return <div className="message">Hubo un problema :(</div>;
     }
-    else if( recipe === null ) {
-      return <div className="message">Hubo un problema :(</div>
-    }
 
-    return <div className="Recipe">
-      <Helmet>
-        <title>{ recipe.name }</title>
-      </Helmet>
+    return (
+      <div className="Recipe">
+        <Helmet>
+          <title>{recipe.name}</title>
+        </Helmet>
 
-      <div className="hero" style={{ backgroundImage: `url(${recipe.thumbnail})` }} />
-      
-      <div className="title">
-        <div className="info">
-          <h1>{ recipe.name }</h1>
-          <p>{ recipe.origin }</p>
+        <div
+          className="hero"
+          style={{ backgroundImage: `url(${recipe.thumbnail})` }}
+        />
+
+        <div className="title">
+          <div className="info">
+            <h1>{recipe.name}</h1>
+            <p>{recipe.origin}</p>
+          </div>
+          <div>
+            <a onClick={this.compartir}>Compartir</a>
+          </div>
         </div>
-        <div>
-        </div>
+
+        <RecipeIngredients ingredients={recipe.ingredients} />
+
+        <RecipeInstructions instructions={recipe.instructions} />
       </div>
-
-
-      <RecipeIngredients ingredients={ recipe.ingredients } />
-
-      <RecipeInstructions instructions={ recipe.instructions } />
-
-    </div>
+    );
   }
-
 }
